06134bcb779be02b882c5d1b5cdf546d026c5a6b
shutsugan <muhammad@nextmedia.ma>
2019-01-19 14:56:37 +0100

    fix (displayerror): get and display the message from the error object

--- client/src/components/DisplayError/index.js
+++ client/src/components/DisplayError/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import './index.css';
 
-const DisplayError = ({error}) => (
-  <div className={`error ${!error ? 'error--disabled' : ''}`}>
-    {error}
-  </div>
-);
+const DisplayError = ({error}) => {
+  console.dir(error);
+  return (
+    <div className={`error ${!error ? 'error--disabled' : ''}`}>
+      {error.message}
+    </div>
+  );
+}
 
 export default DisplayError;
