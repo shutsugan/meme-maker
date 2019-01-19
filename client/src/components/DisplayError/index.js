c9c1ab5c96528621f1df95e9b18dd5313f564d03
shutsugan <muhammad@nextmedia.ma>
2019-01-19 15:06:47 +0100

    refactor (displayerror): clien the code

--- client/src/components/DisplayError/index.js
+++ client/src/components/DisplayError/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 
 import './index.css';
 
-const DisplayError = ({error}) => {
-  console.dir(error);
-  return (
-    <div className={`error ${!error ? 'error--disabled' : ''}`}>
-      {error.message}
-    </div>
-  );
-}
+const DisplayError = ({error}) => (
+  <div className={`error ${!error ? 'error--disabled' : ''}`}>
+    {error.message}
+  </div>
+);
 
 export default DisplayError;
