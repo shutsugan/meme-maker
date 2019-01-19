e0718287d9b9b16f11c0ab38fb2a2726763619e6
shutsugan <muhammad@nextmedia.ma>
2019-01-19 14:43:40 +0100

    style (meme): make meme look like a card

--- client/src/components/Meme/index.js
+++ client/src/components/Meme/index.js
@@ -5,7 +5,7 @@ import './index.css';
 const Meme = ({meme, clicked}) => (
   <div className="meme" onClick={_ => clicked(meme)}>
     <h3 className="meme-title">{meme.title}</h3>
-    <img src={meme.image} alt={meme.title} />
+    <img className="meme-image" src={meme.image} alt={meme.title} />
   </div>
 );
 
